bb039b4b9d7395ad90c266d6849322e215b74a36
Gerardo Acuña <gerardoacuna@gmail.com>
2012-12-12 10:21:28 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vendo::Application.config.secret_token = '750ce20a93fabbccaf5af437c41dd11c181c8b689127d11f2295f8167f8e148fe65b08320bad6cfbddf1aaa1ea97afe3221b0f59baf03b636d49fbf624e0f251'
