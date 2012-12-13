5c83f72af280987f40d4d87710a26b38c8604bd0
Gerardo Acuña <gerardoacuna@gmail.com>
2012-12-12 18:02:31 -0600

    Redirect after signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, alert: exception.message
   end
 
+  def after_sign_in_path_for(resource)
+    case current_user.roles.first.name
+      when 'admin'
+        users_path
+      else
+        admin_content_dashboard_path
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+  	
+  end
+
 end
